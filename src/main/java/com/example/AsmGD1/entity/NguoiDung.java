3f3e2e50ee5f070ce9e409429dd18fbc06244808
PhamNam35 <pnam40249@gmail.com>
2025-09-15 19:45:31 +0700

    them tính năng mới

--- src/main/java/com/example/AsmGD1/entity/NguoiDung.java
+++ src/main/java/com/example/AsmGD1/entity/NguoiDung.java
@@ -28,7 +28,7 @@ public class NguoiDung implements UserDetails { // Thêm "implements UserDetails
     @Column(name = "mat_khau", nullable = false, length = 100)
     private String matKhau;
 
-    @Column(name = "ho_ten", nullable = false, length = 100)
+    @Column(name = "ho_ten", columnDefinition = "NVARCHAR(100)", nullable = false, length = 100 )
     private String hoTen;
 
     @Column(name = "email", nullable = false, length = 100, unique = true)
@@ -38,7 +38,7 @@ public class NguoiDung implements UserDetails { // Thêm "implements UserDetails
     private String soDienThoai;
 
 
-    @Column(name = "vai_tro", length = 50)
+    @Column(name = "vai_tro", columnDefinition = "NVARCHAR(100)", length = 50)
     private String vaiTro; // Ví dụ: "CUSTOMER", "ADMIN", "EMPLOYEE"
 
     @Column(name = "ngay_sinh")
@@ -47,16 +47,16 @@ public class NguoiDung implements UserDetails { // Thêm "implements UserDetails
     @Column(name = "gioi_tinh")
     private Boolean gioiTinh;
 
-    @Column(name = "tinh_thanh_pho", length = 100)
+    @Column(name = "tinh_thanh_pho",columnDefinition = "NVARCHAR(100)", length = 100)
     private String tinhThanhPho;
 
-    @Column(name = "quan_huyen", length = 100)
+    @Column(name = "quan_huyen", columnDefinition = "NVARCHAR(100)",length = 100)
     private String quanHuyen;
 
-    @Column(name = "phuong_xa", length = 100)
+    @Column(name = "phuong_xa",columnDefinition = "NVARCHAR(100)", length = 100)
     private String phuongXa;
 
-    @Column(name = "chi_tiet_dia_chi", columnDefinition = "NVARCHAR(MAX)")
+    @Column(name = "chi_tiet_dia_chi",columnDefinition = "NVARCHAR(100)")
     private String chiTietDiaChi;
 
     @Column(name = "thoi_gian_tao")
@@ -65,6 +65,8 @@ public class NguoiDung implements UserDetails { // Thêm "implements UserDetails
     @Column(name = "trang_thai", nullable = false)
     private Boolean trangThai = true;
 
+    @Column(name = "reset_otp")
+    private String resetOtp;
     @Lob
     @Column(name = "face_descriptor")
     private byte[] faceDescriptor;
@@ -74,9 +76,6 @@ public class NguoiDung implements UserDetails { // Thêm "implements UserDetails
 
     @Column(name = "face_verified")
     private Boolean faceVerified = false;
-
-    @Column(name = "reset_otp")
-    private String resetOtp;
 
     @Column(name = "otp_expiry")
     private LocalDateTime otpExpiry;
